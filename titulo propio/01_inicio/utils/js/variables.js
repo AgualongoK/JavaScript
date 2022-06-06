b9d77d4313b88011888c489b960bf0d5611e7e4f
Agualongo <92918293+AgualongoK@users.noreply.github.com>
2022-06-06 10:09:30 +0200

    update

---
+++ titulo propio/01_inicio/utils/js/variables.js
@@ -0,0 +1,18 @@
+// let --> declaracion de variables de ambito global y de metodo
+// var --> ambito global --> ya no se utiliza
+// const --> constantes 
+
+const DNI = "123456X"; // String
+let nombre = "Borja"; // String
+let nombreObj = new String("Elemento");
+let apellido; // null --> undifined
+let edad = 37; // number
+let carnet =  true; // boolean
+let fecha = new Date(); // daye -->object
+
+apellido = "Martin"; // String
+// DNI = 12345667;
+
+// comprobacion de tipos
+console.log(typeof nombreObj);
+
