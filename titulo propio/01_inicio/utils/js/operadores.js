0f500e56e331908248a9733899a34b8268f3c11c
Agualongo <92918293+AgualongoK@users.noreply.github.com>
2022-06-06 11:14:48 +0200

    update

--- titulo propio/01_inicio/utils/js/operadores.js
+++ titulo propio/01_inicio/utils/js/operadores.js
@@ -69,11 +69,49 @@ console.log(igual);
 // operadores []
 
 // Metodos/funciones
-alert(`Bienvenido ${nombre}`);
+/* alert(`Bienvenido ${nombre}`);
 let confirmacion = confirm("¿Segurp que quieres continuar?");
-console.log(confirmacion);
+console.log(confirmacion); */
 
-prompt("introduce un dato a trabajar");
+// let introduccion = prompt("introduce un dato a trabajar", "parametro por defecto");
+// let introduccion = prompt(`Por favor ${nombre}, introduce un dato a trabajar`, "parametro por defecto");
+
+// console.log(introduccion);
+
+// Ejercicio: realizar un script que te pida nombre y apellidos
+// Una vez introducidos, se pediran dos numeros a operar (cada uno en un promt)
+// En el mensaje, aparecerán los nombres y apellidos introducidos
+// Una vez introducidos los datos se preguntara si se quiere realizar la operacion
+// En el caso de decir que si aparecera un mensaje con todas las operaciones y sus resultados
+// las operaciones entre X e Y son:
+// multiplicacion: xxx
+// division: xxx
+// suma: xxx
+// resta: xxx
+// modulo: xxx
+
+let nombreEj = prompt("Introduzca su nombre");
+let apellidoEj = prompt("Introduzca su apellido");
+
+let numero1Ej = prompt(`Por favor ${nombreEj + ' ' + apellidoEj}, introduce un numero`);
+let numero2Ej = prompt(`Por favor ${nombreEj + ' ' + apellidoEj}, introduce otro numero`);
+
+let pregunta = confirm(`${nombreEj + ' ' + apellidoEj}, ¿quiere realizar alguna operacion?`);
+
+
+suma = numero1Ej + numero2Ej;
+resta = numero1Ej - numero2Ej;
+multiplicacion = numero1Ej * numero2Ej;
+division = numero1Ej / numero2Ej;
+modulo = numero1Ej % numero2Ej;
+
+alert(`La suma de los numeros es ${suma}` + ` | La multiplicaciones de los numeros es ${multiplicacion}` + ` | La resta de los numeros es ${resta}` + ` | La division de los numeros es ${division}` + ` | El modulo de los numeros es ${modulo}`);
+/* alert(`La multiplicaciones de los numeros es ${multiplicacion}`);
+alert(`La resta de los numeros es ${resta}`);
+alert(`La division de los numeros es ${division}`);
+alert(`El modulo de los numeros es ${modulo}`); */
+
+
 
 
 
