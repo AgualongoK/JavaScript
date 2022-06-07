80abc85d2fe641df1a58709bd93fc6757f6f3f2b
Agualongo <92918293+AgualongoK@users.noreply.github.com>
2022-06-07 10:38:07 +0200

    update

--- titulo propio/01_inicio/utils/js/estructuras.js
+++ titulo propio/01_inicio/utils/js/estructuras.js
@@ -1,6 +1,6 @@
 // estructura if / if else
 if (true) {
-    console.log("La respuesta es tru");
+    console.log("La respuesta es true");
 }else{
     console.log("La respuesta es false");
 }
@@ -23,6 +23,7 @@ true & console.log("examen aprobado");
 
 */
 
+/*
 nota = 2;
 switch (nota) {
     case 1:
@@ -37,7 +38,6 @@ switch (nota) {
     /*default:
         console.log("dato incorrecto");
         break;*/
-}
 
 /* 
 Pedir por prompt dos numeros
@@ -49,7 +49,7 @@ Si los numeros introducidos son validos se realizara la operacion indicada
 Si se detecta que las operaciones es negativa el programa parara tras realizar todas las operaciones 
 de los numeros y saltará una alerta indicando de ello
 */
-
+ /*
 let num1 = prompt('Introduzca un numero');
 let num2 = prompt('Introduzca otro numero');
 
@@ -77,4 +77,58 @@ if(!isNaN(Number(num1)) && !isNaN(Number(num2)) && num1 > 0 && num2 > 0){
 
 } else{
     console.log('Alguno de los datos es incorrecto');
+}
+
+*/
+
+/* 
+//estructura del while
+let numero = 12;
+while (numero > 10){
+    console.log("numero mayor que 10");
+    numero--;
+};
+*/
+
+/*
+Ejercicio: pedir un numero por prompt y mostrar por consola todos los numeros
+desde el introducido hasta 0.
+En el caso de ser un numero negativo saltara una alerta.
+*/
+
+let numero1 = prompt("Introduzca un numero");
+
+if(!isNaN(Number(numero1)) && numero1 > 0){
+    while(numero1 > 0){
+        console.log(numero1);
+        numero1--;
+    }
+} else{
+    alert("ERROR");
+}
+
+/*
+Ejercicio: pedir por consola dos numeros y sacar por consola todos los numeros entre ellos
+*/
+
+let numUno = prompt("introduce un numero");
+let numDos = prompt("introduce un numero");
+
+let max, min;
+
+if(numDos > numUno){
+    max = numDos;
+    min = numUno;
+}
+
+else if(numUno > numDos){
+    max = numUno;
+    min = numDos;
+} else{
+    alert("los numeros son iguales");
+}
+
+while(max > min){
+    console.log(max);
+    max--;
 }
