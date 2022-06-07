934c46f5eeefc35a944c14cf96486b637c0d6fa2
Agualongo <92918293+AgualongoK@users.noreply.github.com>
2022-06-07 11:28:54 +0200

    update

--- titulo propio/01_inicio/utils/js/cadenas.js
+++ titulo propio/01_inicio/utils/js/cadenas.js
@@ -4,7 +4,35 @@ console.log(frase);
 
 // Ejercicio: sacar por consola cada una de las letras que tiene la palabra
 
+
+/*
 for (let index = 0; index < frase.length; index++) {
     console.log(frase.charAt(index).toLocaleLowerCase());
 }
+*/
+
+let contadorVocales = 0;
+let contadorConsonantes = 0;
+frase = frase.replaceAll(" ", "");
+for (let index = 0; index < frase.length; index++) {
+    if(
+      frase.charAt(index).toLocaleLowerCase() == 'a' ||
+      frase.charAt(index).toLocaleLowerCase() == 'e' || 
+      frase.charAt(index).toLocaleLowerCase() == 'i' || 
+      frase.charAt(index).toLocaleLowerCase() == 'o' || 
+      frase.charAt(index).toLocaleLowerCase() == 'u'
+      ){
+        contadorVocales++;
+    } else{
+        contadorConsonantes++;
+    }
+}
+
+console.log(`Hay ${contadorVocales} vocales`);
+console.log(`Hay ${contadorConsonantes} consonantes`);
+
+console.log(frase.length);
+
+
+
 
