32fb7278dda5a5d4e0e3e49e2537d6a39def0c01
Agualongo <92918293+AgualongoK@users.noreply.github.com>
2022-06-08 11:05:36 +0200

    update

--- titulo propio/01_inicio/utils/js/arrays.js
+++ titulo propio/01_inicio/utils/js/arrays.js
@@ -2,7 +2,7 @@ let elementos = [1,2,3,4,5,6,7,8,9]; // number
 let equipos = ["madrid","barsa","sevilla","celta","villareal"]; //sring
 let cosas = [1,2,3,4,5,6,"madrid", true]; // any
 
-//acceder posiciones 
+/* //acceder posiciones 
 
 console.log(equipos[0]);
 
@@ -19,5 +19,25 @@ console.log(equipos.push("leganes","cadiz"));
 
 // metodo pop: quita la ultima posicion
 console.log(equipos.pop());
+console.log(equipos); */
+
+// trabajar con el principio
+
+console.log(equipos.unshift("leganes","alcorcon","cadiz"));
 console.log(equipos);
+
+// metodo shift: elimina el primer elemento del array
+console.log(equipos.shift());
+console.log(equipos);
+
+// borrar un elemento
+    // foreach --> if busco el elemento y lo igualo a undifined
+    // metodo filter --> filtrar por x condiciones
+
+equipos.filter((item) => {
+    return (item != "sevilla" || item.length == 5);
+});
+
+console.log(equipos);
+
 
---
+++ titulo propio/01_inicio/utils/js/arraysEj.js
@@ -0,0 +1,55 @@
+let array = [1,2,3,4,5,6,7,8,9,10];
+let contador = 0;
+
+/* 
+Iterar por todos los elementos dentro de un array utilizando while y mostrarlos en pantalla.
+while (contador < array.length) {
+    for (let index = 0; index < array.length; index++) {
+        console.log(array[index]);
+        contador++;
+    }
+}
+
+Iterar por todos los elementos dentro de un array utilizando for y mostrarlos en pantalla.
+for (let index = 0; index < array.length; index++) {
+    console.log(array[index]);
+    
+}
+Iterar por todos los elementos dentro de un array utilizando .forEach y mostrarlos en pantalla.
+array.forEach(element => {
+    console.log(array[element-1]);   
+});
+
+Mostrar todos los elementos dentro de un array sumándole uno a cada uno.
+for (let index = 0; index < array.length; index++) {
+    console.log((array[index]) + 1);
+}
+
+Calcular la media de todos los elementos del array
+let total = 0;
+let media = 0.0;
+for (let index = 0; index < array.length; index++) {
+    total = total + (array[index]);
+}
+
+media = total / array.length;
+
+console.log(total);
+console.log(media);
+
+
+Crear un array vacío, luego generar 20 números al azar y guardarlos en un array.
+let arrayVacio = [null];
+
+let aleatorio = 0;
+
+for (let index = 0; index < 20; index++) {
+    aleatorio = (Math.random()*100);
+    arrayVacio[index] = aleatorio.toFixed(2);
+}
+
+for (let index = 0; index < arrayVacio.length; index++) {
+    console.log(arrayVacio[index]);
+}
+
+*/
